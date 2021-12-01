479621988afdaeac0157dfb967a25b1143b82e90
Ville Andersson <winstoni@windowslive.com>
2021-12-01 02:20:41 +0200

    texttospeech

--- App.js
+++ App.js
@@ -1,12 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View, Button, TextInput } from "react-native";
+import * as Speech from "expo-speech";
 
 export default function App() {
+  const [puhe, setPuhe] = React.useState("");
+
+  const speak = () => {
+    Speech.speak(puhe);
+  };
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TextInput
+        style={{
+          fontSize: 18,
+          width: 200,
+          borderColor: "black",
+          borderWidth: 1,
+          marginBottom: 15,
+        }}
+        placeholder="Input text"
+        onChangeText={(text) => setPuhe(text)}
+      />
+      <Button title="Press to hear text" onPress={speak} />
     </View>
   );
 }
@@ -14,8 +30,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
